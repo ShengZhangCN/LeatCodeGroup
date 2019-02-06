b19bd167c2cb9ec00b0329678094f6225da9bf83
sheng zhang <shengzcn@gmail.com>
2019-02-05 21:51:36 -0500

    first test file

---
+++ Python/Easy/1TwoSum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # dict = {}
+        # for i, v in enumerate(nums):
+        #     if target-v in dict:
+        #         return [dict[target-v], i]
+        #     dict[v] = i
+
+        dict = {}
+        for i, v in enumerate(nums):
+            if v in dict:
+                return [dict[v], i]
+            dict[target - v] = i
