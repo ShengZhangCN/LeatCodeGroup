e12ac6d4c2fd279679fd8260b09fca3eb5c5ec4a
Sheng Zhang <shengzcn@gmail.com>
2019-04-18 23:49:29 -0400

    Create shengzhang_solution.py

---
+++ 590_N-aryTreePostorderTraversal/shengzhang_solution.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def preorder(self, root):
+        def preorder_helper(root):
+            trav.append(root.val)
+            for child in root.children:
+                preorder_helper(child)
+
+        trav = []
+        if not root:
+            return []
+        preorder_helper(root)
+        return trav
